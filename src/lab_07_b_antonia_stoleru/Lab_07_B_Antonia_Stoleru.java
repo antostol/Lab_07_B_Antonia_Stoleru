c6fafb30f5929533f94c6e5539468f2d08f055a3
Antonia Stoleru <2484936@edu.vaniercollege.qc.ca>
2025-10-29 09:58:33 -0400

    original lab structure added

--- src/lab_07_b_antonia_stoleru/Lab_07_B_Antonia_Stoleru.java
+++ src/lab_07_b_antonia_stoleru/Lab_07_B_Antonia_Stoleru.java
@@ -4,17 +4,54 @@
  */
 package lab_07_b_antonia_stoleru;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.image.*;
+import java.util.Random;
+
 /**
  *
  * @author 2484936
  */
-public class Lab_07_B_Antonia_Stoleru {
+public class Lab_07_B_Antonia_Stoleru extends Application {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+       BorderPane root = new BorderPane();
+       Scene sc = new Scene(root, 250, 300);
+       stage.setTitle("Java Games");
+       
+       Label lblImage = new Label("");
+       StackPane middle = new StackPane(lblImage);
+       Label top = new Label("Random game");
+       Label bottom = new Label("Waiting...");
+       
+       root.setTop(top);
+       root.setBottom(bottom);
+       root.setCenter(middle);
+       
+       Random r = new Random();
+       int rand = r.nextInt(20) + 101;
+       
+       String link = "file:images/" + rand + ".jpg";
+       
+       Image image = new Image(link);
+       
+       lblImage.setGraphic(new ImageView(image));
+       
+       stage.setScene(sc);
+       stage.show();
     }
     
 }
